a32273712e960e925dac6a3014ab84146ba91c0e
Owynn <angelaleman2212@gmail.com>
2025-05-24 15:38:12 -0600

    Organization and discord modal popUp working

--- myScript.js
+++
@@ -1,7 +0,0 @@
-const btnOpenModal = document.querySelector("#open-modal");
-const btnCloseModal = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-
-btnOpenModal.addEventListener("click", () =>{
-modal.show();
-})
