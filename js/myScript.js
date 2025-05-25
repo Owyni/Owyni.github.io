da7437b926ef46b39e2cfe08194ece645d38531d
Owynn <angelaleman2212@gmail.com>
2025-05-25 12:42:27 -0600

    Dark mode, and some fixes

--- js/myScript.js
+++ js/myScript.js
@@ -11,3 +11,17 @@ closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.remove('modal--show');
 })
+
+//DarkMode
+function lightDarkMode(){
+    const darkMode = document.querySelector(".dark-mode i")
+    const elements = document.querySelectorAll("body, section, nav a")
+
+    darkMode.classList.toggle("fa-sun")
+    darkMode.classList.toggle("fa-moon")
+    darkMode.style.color = darkMode.classList.contains("fa-moon") ? "white" : "black";
+
+    elements.forEach(element =>
+        element.classList.toggle("dark-mode-class")
+    );
+}
